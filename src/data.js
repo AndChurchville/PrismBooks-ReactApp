7fc7a63681696457ba16dea19a3b569b2fb2081d
AndChurchville <48935849+AndChurchville@users.noreply.github.com>
2020-04-27 23:49:33 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,113 @@
+
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "The Art of Spider-Man: Into the Spider-Verse",
+    img: "img/bookproduct-1.jpg",
+    price: 25,
+    company: "Marvel",
+    info:
+      "Discover the world of the multi-award-winning Spider-Man: Into the Spider-Verse in this stunning collection of art. Packed with concept art, final designs, and artist commentary plus previously unseen storyboards.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "The Art of Cuphead",
+    img: "img/bookproduct-2.jpg",
+    price: 26,
+    company: "Studio MDHR",
+    info:
+      "Get transported back to the golden age of 1930s animation with an art book celebrating the acclaimed run & gun game, Cuphead!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "The Art of Steven Universe: The Movie",
+    img: "img/bookproduct-3.jpg",
+    price: 8,
+    company: "Cartoon Network",
+    info:
+      "This magical deep-dive into Steven Universe The Movie is designed by Ryan Sands (Frontier) in conjunction with Steven Universe creator Rebecca Sugar! See preliminary character designs, witness the formation of settings and storyboards, and discover the art that shaped the full-length movie! It's a new kind of artistic adventure with with Garnet, Amethyst, Pearl, and--of course--Steven.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: " The Art of Avatar: The Last Airbender",
+    img: "img/bookproduct-4.jpg",
+    price: 25,
+    company: "Nickelodeon",
+    info:
+      "Since its debut in 2005, Avatar: The Last Airbender has remained one of Nickelodeon's most talked about and highest-rated animated series to date, and it's not too hard to see why... Not only does it offer an epic storyline, engaging characters, action, adventure, and a powerful message about the importance of hope in a world long-besieged by war, all the while serving up top-notch entertainment. It's also beautifully executed, a stunning masterpiece of animated storytelling that harmonizes Western influences with aesthetic inspiration drawn from a diverse array of cultures, including those of China, Japan, India, and the Inuit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Perspective! for Comic Book Artists",
+    img: "img/bookproduct-5.jpg",
+    price: 15,
+    company: "David Chelsea",
+    info:
+      "This clever book teaches artists the unique skill of drawing perspective for spectacular landscapes, fantastic interiors, and other wildly animated backgrounds to fit comic-strip panels.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Successful Drawing",
+    img: "img/bookproduct-6.jpg",
+    price: 17,
+    company: "Andrew Loomis",
+    info:
+      "The illustrator Andrew Loomis is revered amongst artists for his mastery of drawing technique and his clean, realist style. His hugely influential series of art instruction books have never been bettered and Successful Drawing, the third in Titan's programme of facsimile editions, returns this classic title to print for the first time in decades.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "FORCE: Dynamic Life Drawing",
+    img: "img/bookproduct-7.jpg",
+    price: 30,
+    company: "Mike Mattesi",
+    info:
+      "Bring your artwork to life with the power of the FORCE! Watch, listen, and follow along as Mike Mattesi demonstrates the fundamental FORCE line and explains dynamic figure drawing techniques through 30 videos that are launched through the book's companion App. Packed with superb, powerfully drawn examples, the updated third edition of FORCE features an all-new section on the FORCE blob and dozens of fresh illustrations. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Happy Abstracts: Fearless Painting for True Beginners! ",
+    img: "img/bookproduct-8.jpg",
+    price: 17,
+    company: "EttaVee",
+    info:
+      "Happy Abstracts is an explorative painting guide by colorful artist EttaVee, (Jessi Raulet) where you’ll learn to create bright and uplifting abstract art on canvas. Burn bright and beautiful with bold hues and daring brushwork as you paint along with stroke-by-stroke photo guides. Journey through six projects, where you’ll be encouraged to explore abstract painting with a playful and open mind.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
